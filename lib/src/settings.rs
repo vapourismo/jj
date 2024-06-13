65ebc01ef1f5a2a1a6ecb8c19939f1a1308dc60b
Ole Krüger <ole.kruger@trili.tech>
2024-06-13 19:15:33 +0100

    git: allow local branches to be protected

--- lib/src/settings.rs
+++ lib/src/settings.rs
@@ -66,6 +66,7 @@ pub struct GitSettings {
     #[cfg(feature = "git2")]
     pub subprocess: bool,
     pub executable_path: PathBuf,
+    pub protect_local_branches: bool,
 }
 
 impl GitSettings {
@@ -76,6 +77,7 @@ impl GitSettings {
             #[cfg(feature = "git2")]
             subprocess: settings.get_bool("git.subprocess")?,
             executable_path: settings.get("git.executable-path")?,
+            protect_local_branches: settings.get_bool("git.protect-local-branches")?,
         })
     }
 }
@@ -88,6 +90,7 @@ impl Default for GitSettings {
             #[cfg(feature = "git2")]
             subprocess: true,
             executable_path: PathBuf::from("git"),
+            protect_local_branches: false,
         }
     }
 }
